b5927722a66dee6ab5dd56a89174aa43b4b0d0bc
Abhinaw Ratan <75108723+AbhinawRatan@users.noreply.github.com>
2023-03-03 01:27:50 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
 import React from 'react';
 import styles from './style.js';
-import {NavBar,Feedback,Footer,Working,Hero,Zk} from './componets';
+import { NavBar, Feedback, Footer, Working, Hero, Zk } from './componets';
 import Ipfs from './componets/Ipfs.jsx';
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
-
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import Dashboard from './Page/Dashboard.jsx';
+
 const App = () => {
   return (
-    <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Router>
-          <Routes>
-          <Route path='Dashboard'element={<Dashboard/>}/>
-          </Routes>
-        </Router>
-        <NavBar />
-        
-      </div>
-    </div>
+    <Router>
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <NavBar />
+            <Routes>
+              <Route path='/dashboard' element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Working />
+            <Feedback />
+            <Zk />
+            <Ipfs />
+            <Footer />
+          </div>
+        </div>
       </div>
-    </div>
-    
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-      <Footer/>
-      <Working/>
-      <Feedback/>
-      <Zk/>
-      <Ipfs/>
-      </div>
-    </div>
-  </div>
-);
+    </Router>
+  );
 };
 
-export default App
+export default App;
--- src/constant/index.js
+++ src/constant/index.js
@@ -13,6 +13,10 @@ export const navLinks = [
     id: "Working",
     title: "Working",
   },
+  {
+    id: "ipfs",
+    title: "Ipfs",
+  },
   
 ];
 
@@ -85,71 +89,7 @@ export const stats = [
   },
 ];
 
-export const footerLinks = [
-  {
-    title: "Useful Links",
-    links: [
-      {
-        name: "Content",
-        link: "https://www.hoobank.com/content/",
-      },
-      {
-        name: "How it Works",
-        link: "https://www.hoobank.com/how-it-works/",
-      },
-      {
-        name: "Create",
-        link: "https://www.hoobank.com/create/",
-      },
-      {
-        name: "Explore",
-        link: "https://www.hoobank.com/explore/",
-      },
-      {
-        name: "Terms & Services",
-        link: "https://www.hoobank.com/terms-and-services/",
-      },
-    ],
-  },
-  {
-    title: "Community",
-    links: [
-      {
-        name: "Help Center",
-        link: "https://www.hoobank.com/help-center/",
-      },
-      {
-        name: "Partners",
-        link: "https://www.hoobank.com/partners/",
-      },
-      {
-        name: "Suggestions",
-        link: "https://www.hoobank.com/suggestions/",
-      },
-      {
-        name: "Blog",
-        link: "https://www.hoobank.com/blog/",
-      },
-      {
-        name: "Newsletters",
-        link: "https://www.hoobank.com/newsletters/",
-      },
-    ],
-  },
-  {
-    title: "Partner",
-    links: [
-      {
-        name: "Our Partner",
-        link: "https://www.hoobank.com/our-partner/",
-      },
-      {
-        name: "Become a Partner",
-        link: "https://www.hoobank.com/become-a-partner/",
-      },
-    ],
-  },
-];
+
 
 export const socialMedia = [
   {
@@ -174,21 +114,3 @@ export const socialMedia = [
   },
 ];
 
-export const clients = [
-  {
-    id: "client-1",
-    logo: airbnb,
-  },
-  {
-    id: "client-2",
-    logo: binance,
-  },
-  {
-    id: "client-3",
-    logo: coinbase,
-  },
-  {
-    id: "client-4",
-    logo: dropbox,
-  },
-];
