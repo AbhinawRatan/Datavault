a7f740f0c604f2949c93665a1ef455dd1daa5af1
Abhinaw Ratan <75108723+AbhinawRatan@users.noreply.github.com>
2023-03-07 17:15:40 +0530

    dashboard fix

--- client/src/Page/DashboardMain.jsx
+++ client/src/Page/DashboardMain.jsx
@@ -1,25 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HiMenuAlt3 } from "react-icons/hi";
 import { MdOutlineDashboard } from "react-icons/md";
-import { AiOutlineUser,AiOutlineShareAlt, AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser, AiOutlineShareAlt, AiOutlineHome } from "react-icons/ai";
 import { FiFolder, FiUpload } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import FileUpload from "../componets/FileUpload";
+import Upload from "../../../artifacts/contracts/Upload.sol/Upload.json";
+import { ethers } from "ethers";
 
+const DashboardMain = () => {
+  const [account, setAccount] = useState("");
+  const [contract, setContract] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [files, setFiles] = useState([{ name: "myFile.pdf" }]);
 
-const Home = () => {
+  useEffect(() => {
+    const loadProvider = async () => {
+      if (window.ethereum) {
+        window.ethereum.autoRefreshOnNetworkChange = false;
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        setAccount(address);
+
+        let contractAddress = "0x3078157fec210C5BAb87031205FDA3877C150C1f";
+
+        const contract = new ethers.Contract(
+          contractAddress,
+          Upload.abi,
+          signer
+        );
+        console.log(contract)
+        setContract(contract);
+      } else {
+        console.error("Metamask is not installed");
+      }
+    };
+
+    loadProvider();
+  }, []);
+
   const menus = [
-    { path:"/", name: "Home", link: "/", icon: AiOutlineHome },
-    { path:"/YourAccount" ,name: "Your Account", link: "/", icon: AiOutlineUser , margin: true},
-    { path:"/Upload", name: "Upload", link: "/", icon: FiUpload, margin: true },
-    { path:"/FileManager",name: "File Manager", link: "/", icon: FiFolder, margin: true},
-    { path:"/Shared",name: "Shared", link: "/", icon: AiOutlineShareAlt, margin: true },
-
+    { path: "/", name: "Home", link: "/", icon: AiOutlineHome },
+    {
+      path: "/YourAccount",
+      name: "Your Account",
+      link: "/",
+      icon: AiOutlineUser,
+      margin: true,
+    },
+    { path: "/Upload", name: "Upload", link: "/", icon: FiUpload, margin: true },
+    {
+      path: "/FileUpload",
+      name: "File Manager",
+      link: "/",
+      icon: FiFolder,
+      margin: true,
+    },
+    { path: "/Shared", name: "Shared", link: "/", icon: AiOutlineShareAlt, margin: true },
   ];
+
   const [open, setOpen] = useState(true);
-  const [files, setFiles] = useState([{
-    name: 'myFile.pdf'
-  }])
+
   return (
     <section className="flex gap-6">
       <div
@@ -65,18 +108,19 @@ const Home = () => {
           ))}
         </div>
       </div>
-      <div className=" m-3 text-xl text-gray-900 font-semibold w-full flex-2">
-      <span className="text-gradient font-poppins font-semibold ss:text-[72px] text-[40px] text-white ss:leading-[100.8px] leading-[75px]">Upload
-            <div className="absolute z-[1] w-[50%] h-[50%] rounded-full blue__gradient bottom-70" />
-      </span>{" "}
-      <h4 className="text-black text-right -y-10">Hey There!</h4>
-      <div>
-      <p className="title">Upload File</p>
-      <FileUpload files ={files} setFiles={setFiles}/>
-      </div>
+      <div className="m-3 text-xl text-gray-900 font-semibold w-full flex-2">
+        <span className="text-gradient font-poppins font-semibold ss:text-[72px] text-[40px] text-white ss:leading-[100.8px] leading-[75px]">
+          Upload
+          <div className="absolute z-[1] w-[50%] h-[50%] rounded-full blue__gradient bottom-70" />
+        </span>{" "}
+        <h4 className="text-black text-right -y-10">Hey There!</h4>
+        <div>
+          <p className="title">Upload File</p>
+          <FileUpload files={files} setFiles={setFiles} />
+        </div>
       </div>
     </section>
   );
 };
 
-export default Home;
+export default DashboardMain;
