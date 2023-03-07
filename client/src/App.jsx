cef2b700f65149073efa57bb98156e1d06145bf5
Abhinaw Ratan <75108723+AbhinawRatan@users.noreply.github.com>
2023-03-07 17:17:29 +0530

    dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink, BrowserRouter } from 'react-router-dom';
-import DashboardMain from './Page/DashboardMain.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DashboardMain from './Page/DashboardMain';
 import Home from './Page/Home';
-import YourAccount from './componets/YourAccount'
+import YourAccount from './componets/YourAccount';
+import Shared from './componets/Shared';
+import FileUpload from './componets/FileUpload';
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' exact element={<Home/>} />
-      <Route path='/Dashboard' exact element={<DashboardMain/>} />
-    </Routes>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/Dashboard' element={<DashboardMain />} />
+        <Route path='/Upload' element={<DashboardMain />} />
+        <Route path='/YourAccount' element={<YourAccount />} />
+        <Route path='/Shared' element={<Shared />} />
+        <Route exact path='/FileUpload' element={<FileUpload />} />
+      </Routes>
     </BrowserRouter>
   );
 };
